3d564c34aa286f73d2734ba5d577f0c7b962a9f5
ketu9032 <ketu9032@gmail.com>
2022-03-10 15:52:35 +0530

    sidebar permission added

--- frontend/src/app/layout/components/sidebar/sidebar.component.ts
+++ frontend/src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,23 +1,122 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/shared/services';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  public showMenu: string;
-  constructor() {}
+    public showMenu: string;
+    userPermission: any;
+    constructor(private restService: RestService) { }
 
-  ngOnInit() {
-    this.showMenu = '';
-  }
+    menu: { link: string, name: string, icon: string, key: string }[] = [
+        {
+            link: '/dashboard',
+            name: 'Dashboard',
+            icon: 'dashboard',
+            key: 'dashboard'
+        },
+        {
+            link: '/user',
+            name: 'User',
+            icon: 'people',
+            key: 'users'
+        },
+        {
+            link: '/cdf',
+            name: 'CDF',
+            icon: 'person',
+            key: 'cdf'
+        },
+        {
+            link: '/customers',
+            name: 'Customers',
+            icon: 'people',
+            key: 'customers',
+        },
+        {
+            link: '/items',
+            name: 'Items',
+            icon: 'list',
+            key: 'items'
+        },
+        {
+            link: '/salesQuotation',
+            name: 'Sales Quotation',
+            icon: 'bookmark_border',
+            key: 'sales_quotation'
+        },
+        {
+            link: '/sales',
+            name: 'Sales',
+            icon: 'payment',
+            key: 'sales'
+        },
+        {
+            link: '/suppliers',
+            name: 'Suppliers',
+            icon: 'bookmark_border',
+            key: 'suppliers'
 
-  addExpandClass(element: any) {
-    if (element === this.showMenu) {
-      this.showMenu = '0';
-    } else {
-      this.showMenu = element;
+        },
+        {
+            link: '/purchaseQuotation',
+            name: 'Purchase Quotation',
+            icon: 'payment',
+            key: 'purchase_quotation'
+        },
+        {
+            link: '/purchase',
+            name: 'Purchase',
+            icon: 'payment',
+            key: 'purchase'
+        },
+        {
+            link: '/expense',
+            name: 'Expense',
+            icon: 'attach_money',
+            key: 'expense'
+        },
+        {
+            link: '/transfer',
+            name: 'Transfer',
+            icon: 'attach_money',
+            key: 'transfer'
+        },
+        {
+            link: '/analysis',
+            name: 'Analysis',
+            icon: 'timeline',
+            key: 'analysis'
+        },
+        {
+            link: '/rojMed',
+            name: 'Roj-Med',
+            icon: 'money',
+            key: 'roj_med'
+
+        },
+        {
+            link: '/history',
+            name: 'History',
+            icon: 'history',
+            key: 'history'
+        }
+    ]
+
+    ngOnInit() {
+        this.showMenu = '';
+        const user = this.restService.getUserData();
+        this.userPermission = user.permission;
     }
-  }
+
+    addExpandClass(element: any) {
+        if (element === this.showMenu) {
+            this.showMenu = '0';
+        } else {
+            this.showMenu = element;
+        }
+    }
 }
