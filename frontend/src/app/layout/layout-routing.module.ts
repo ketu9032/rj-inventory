8fd3d4bc5fb3b2a1b5f46f3ca91e936c96f9e6ea
ketu9032 <ketu9032@gmail.com>
2022-03-12 11:18:25 +0530

    permission

--- frontend/src/app/layout/layout-routing.module.ts
+++ frontend/src/app/layout/layout-routing.module.ts
@@ -1,98 +1,116 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/guard';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard'
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
-      },
-      {
-        path: 'user',
-        loadChildren: () =>
-          import('./user/user.module').then((m) => m.UserModule)
-      },
-      {
-        path: 'cdf',
-        loadChildren: () =>
-          import('./cdf/cdf.module').then((m) => m.CDFModule)
-      },
-      {
-        path: 'customers',
-        loadChildren: () =>
-          import('./customers/customers.module').then((m) => m.CustomersModule)
-      },
-      {
-        path: 'items',
-        loadChildren: () =>
-          import('./items/items.module').then((m) => m.ItemsModule)
-      },
-      {
-        path: 'sales',
-        loadChildren: () =>
-          import('./sales/sales.module').then((m) => m.SalesModule)
-      },
-      {
-        path: 'salesQuotation',
-        loadChildren: () =>
-          import('./sales-quotation/sales-quotation.module').then((m) => m.SalesQuotationModule)
-      },
-      {
-        path: 'suppliers',
-        loadChildren: () =>
-          import('./suppliers/suppliers.module').then((m) => m.SuppliersModule)
-      },
-      {
-        path: 'purchaseQuotation',
-        loadChildren: () =>
-          import('./purchase-quotation/purchase-quotation.module').then((m) => m.PurchaseQuotationModule)
-      },
-      {
-        path: 'purchase',
-        loadChildren: () =>
-          import('./purchase/purchase.module').then((m) => m.PurchaseModule)
-      },
-      {
-        path: 'expense',
-        loadChildren: () =>
-          import('./expense/expense.module').then((m) => m.ExpenseModule)
-      },
-      {
-        path: 'transfer',
-        loadChildren: () =>
-          import('./transfer/transfer.module').then((m) => m.TransferModule)
-      },
-      {
-        path: 'analysis',
-        loadChildren: () =>
-          import('./analysis/analysis.module').then((m) => m.AnalysisModule)
-      },
-      {
-        path: 'rojMed',
-        loadChildren: () =>
-          import('./roj-med/roj-med.module').then((m) => m.RojMedModule)
-      },
-      {
-        path: 'history',
-        loadChildren: () =>
-          import('./history/history.module').then((m) => m.HistoryModule)
-      },
+    {
 
-    ]
-  }
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard'
+            },
+            {
+                path: 'dashboard',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+            },
+            {
+                path: 'user',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./user/user.module').then((m) => m.UserModule)
+            },
+            {
+                path: 'cdf',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./cdf/cdf.module').then((m) => m.CDFModule)
+            },
+            {
+                path: 'customers',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./customers/customers.module').then((m) => m.CustomersModule)
+            },
+            {
+
+                path: 'items',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./items/items.module').then((m) => m.ItemsModule)
+            },
+            {
+                path: 'sales',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./sales/sales.module').then((m) => m.SalesModule)
+            },
+            {
+                path: 'salesQuotation',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./sales-quotation/sales-quotation.module').then((m) => m.SalesQuotationModule)
+            },
+            {
+                path: 'suppliers',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./suppliers/suppliers.module').then((m) => m.SuppliersModule)
+            },
+            {
+                path: 'purchaseQuotation',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./purchase-quotation/purchase-quotation.module').then((m) => m.PurchaseQuotationModule)
+            },
+            {
+                path: 'purchase',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./purchase/purchase.module').then((m) => m.PurchaseModule)
+            },
+            {
+                path: 'expense',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./expense/expense.module').then((m) => m.ExpenseModule)
+            },
+            {
+                path: 'transfer',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./transfer/transfer.module').then((m) => m.TransferModule)
+            },
+            {
+                path: 'analysis',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./analysis/analysis.module').then((m) => m.AnalysisModule)
+            },
+            {
+                path: 'rojMed',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./roj-med/roj-med.module').then((m) => m.RojMedModule)
+            },
+            {
+                path: 'history',
+                canActivate: [AuthGuard],
+                loadChildren: () =>
+                    import('./history/history.module').then((m) => m.HistoryModule)
+            },
+
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class LayoutRoutingModule {}
+export class LayoutRoutingModule { }
--- frontend/src/app/shared/guard/auth.guard.ts
+++ frontend/src/app/shared/guard/auth.guard.ts
@@ -1,17 +1,48 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { Injectable, OnInit } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RestService } from '../services/rest.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+    user: any;
 
-  canActivate() {
-    if (localStorage.getItem('user')) {
-      return true;
+    constructor(private router: Router, private restService: RestService) { }
+
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot) {
+
+
+        this.user = this.restService.getUserData();
+
+        if (localStorage.getItem('user')) {
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+         if( state.url  === this.user.permission.dashboard){
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+         if( state.url  === this.user.permission.user){
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+         if( state.url  === this.user.permission.cdf){
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+         if( state.url  === this.user.permission.customers){
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+
+        return false;
+
     }
-
-    this.router.navigate(['/login']);
-    return false;
-  }
 }
