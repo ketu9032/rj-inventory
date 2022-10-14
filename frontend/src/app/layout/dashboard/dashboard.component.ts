3f24daeadb3ee9da2770a0daf7c7256211b48f2d
baj9032 <baj9032@hotmail.com>
2022-10-14 17:22:30 +0530

    dashboard ui work

--- frontend/src/app/layout/dashboard/dashboard.component.ts
+++ frontend/src/app/layout/dashboard/dashboard.component.ts
@@ -9,6 +9,25 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+    loader: boolean = false;
+    invoice: number = 9999999;
+    sales: number = 9999999;
+    profit: number = 9999999;
+    payment: number = 9999999;
+    qty: number = 9999999;
+    expense: number = 9999999;
+    startDate: any;
+    endDate: any;
+    totalSaleInDayWise: number = 0;
+    totalProfitInDayWise: number = 0;
+    averageSaleInDayWise: number = 0;
+    averageProfitInDayWise: number = 0;
+    customer: number = 0;
+    supplier: number = 0;
+    product: number = 0;
+    companyBalance: number = 2021250450;
+
+
   ngOnInit() { }
 
   constructor(
